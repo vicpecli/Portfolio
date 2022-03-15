c30a719a4061517389859606465c690f4edb7b76
Victor Pellicer <Victorpellicer96@hotmail.com>
2022-03-15 21:12:48 +0100

    Resuelto JS

--- js/index.js
+++ js/index.js
@@ -109,25 +109,25 @@ $('.navbar a').on('click', function(e){
 
         $("html, body").animate({
             scrollTop: posicion-minusposition
-        }, 10000); 
+        }, 2000); 
     }
 
 });
 
 
 
-// $(document).ready(function(){
-//     var y = $(this).scrollTop()
-//     if(y>0){
-//         console.log('Soy mayor', y)
-//         $("html, body").animate({
-//             scrollTop: 0
-//         }, 3000);
-//     }else{
-//         console.log('Soy igual', y)
-//     }
+$(document).ready(function(){
+    var y = $(this).scrollTop()
+    if(y>0){
+        console.log('Soy mayor', y)
+        $("html, body").animate({
+            scrollTop: 0
+        }, 3000);
+    }else{
+        console.log('Soy igual', y)
+    }
            
-//  })
+ })
 
 
 
